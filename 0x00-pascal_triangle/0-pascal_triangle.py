0ee0efc57f6687bcd0a0e511f28b5d411d8c37e2
Precious Peter <peterprecious370@gmail.com>
2024-05-30 21:21:29 -0700

    first interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle interview
+"""
+
+
+def pascal_triangle(n):
+    """
+    Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
+    """
+    resp = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            resp.append(level)
+    return resp
